509d46c08ddde25a289a7e21d7e23c28ea6bfe9b
EC2 Default User <ec2-user@ip-172-31-93-183.ec2.internal>
2019-07-31 09:33:57 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-var http = require('http');
-var settings = require('./settings');
-var server = http.createServer();
+let express = require("express")
+let app = express()
 
-var msg;
-server.on('request',function(req,res){
-    
-    switch (req.url){
-        case '/about':
-            msg = "About - about this web site"
-            break;
-        case '/profile':
-            msg = "Profile - about me."
-            break;
-        default:
-            msg = 'Wrong Page - please check URL.';
-            break;
-    }
-    
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(msg);
-    res.end();
-});
-server.listen(settings.port,settings.host);
-console.log("server listening ...");
+app.get('/', (req, res)=>{
+    res.send("Hello, World")
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server listening")
+})
